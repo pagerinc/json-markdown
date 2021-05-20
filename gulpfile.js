e93590a30679a3b26162ad2810edaaf179336739
Jugal Shah <jugaltheshah@users.noreply.github.com>
2021-05-20 16:07:25 -0400

    chore: remove unnecessary upstream changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,6 @@
 
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
-const updateCheck = require('update-notifier');
-const packageJSON = require('./package.json');
-const execSync = require('child_process').execSync;
 
 gulp.task('lint', () => {
   return gulp.src(['src/**/*.js'])
@@ -16,27 +13,5 @@ gulp.task('lint', () => {
 gulp.task('watch', ['lint'], () => {
   return gulp.watch(paths.scripts, ['test']);
 });
-
-gulp.task('deploy', () => {
-  updateCheck({
-    pkg: packageJSON,
-    updateCheckInterval: 0,
-    callback: (err, update) => {
-      if (err) {
-        return err;
-      }
-      if (update.current !== update.latest) {
-        try {
-          execSync('npm publish');
-        } catch (error) {
-          return err;
-        }
-      } else {
-        return console.log('Nothing to deploy.');
-      }
-    }
-  });
-});
-
 
 gulp.task('default', ['lint']);
--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ([ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ])
-then
-  gulp deploy
-  echo 'Package build and published'
-else
-  echo "Build successful, but not publishing!"
-fi
